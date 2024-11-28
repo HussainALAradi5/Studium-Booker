3ce5ad0d70080de794040968700758260340b3cb
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-11-28 23:19:32 +0300

    some more enhacemenet at the user table)

--- db/database.php
+++ db/database.php
@@ -32,8 +32,10 @@ try {
   $pdo->exec("CREATE TABLE IF NOT EXISTS user (
         user_id INT AUTO_INCREMENT PRIMARY KEY,
         user_name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
         password_digest VARCHAR(255) NOT NULL,
-        is_admin BOOLEAN NOT NULL DEFAULT 0
+        is_admin BOOLEAN NOT NULL DEFAULT 0,
+        is_active BOOLEAN NOT NULL DEFAULT 1
     )");
 
   // Reservation table
