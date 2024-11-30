5cadafb17351b57865ba66bc78bb36b538ec1c2a
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-11-30 15:32:28 +0300

    some minor user friendly changes

--- views/user/user_details.php
+++ views/user/user_details.php
@@ -1,8 +1,14 @@
 <?php
 $user = get_user_details();
 if ($user) {
+
   echo "<h1>Welcome, " . htmlspecialchars($user['user_name']) . "!</h1>";
   echo "<p>Email: " . htmlspecialchars($user['email']) . "</p>";
+  echo "<p>User Name: " . htmlspecialchars($user['user_name']) . "</p>";
+  if ($user['is_active'] == 1)
+    echo "<p>Status: Active </p>";
+  else
+    echo "<p>Status: InActive </p>";
 } else {
   echo "<h1>No user found</h1>";
   echo "<p>You must be logged in to view your profile. <a href='index.php?action=login'>Login here</a></p>";
