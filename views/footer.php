72e13bf0329c2a5851f682176b1e56a9a8884fce
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-12-14 23:30:53 +0300

    make the index,header,footer page repsonsive

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,9 @@
-<footer class="text-center py-4">
+<footer class="text-center py-4 fixed-bottom">
   <div class="container">
     <h2>&copy; <?php echo date('Y'); ?> studium Booker. All rights reserved.</h2>
   </div>
 </footer>
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
